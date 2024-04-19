b078d15589be73ebdf298809368f5c270481ab85
Hugo Hirsch <github@hugo-hirsch.de>
2024-04-19 22:39:39 +0200

    Introduce new template

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<meta name="Robots" content="NOINDEX,NOFOLLOW"/>
+		<meta name="DC.Language" content="de"/>
+		<meta name="DC.Date" content="2024-04-19"/>
+		<title>https://www.hugo-hirsch.de - Willkommen im Wald</title>
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<h1>Willkommen bei Hugo Hirsch</h1>
+				<p>A simple template for telling the world when you'll launch<br />
+				your next big thing. Brought to you by <a href="http://html5up.net">HTML5 UP</a>.</p>
+			</header>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<ul class="icons">
+					<li><a href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
+				</ul>
+				<ul class="copyright">
+					<li>&copy; Untitled.</li><li>Credits: <a href="http://html5up.net/eventually">Eventually by HTML5 UP</a></li>
+				</ul>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/main.js"></script>
+	</body>
+</html>
